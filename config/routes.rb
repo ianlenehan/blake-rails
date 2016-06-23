01189b47922c8c6c7ceca0905c5690ba096e9acc
Ian Lenehan <ianlenehan@gmail.com>
2016-06-23 11:35:02 +1000

    preparing for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   get 'marks/index'
 
-  get '/marks/:student_id/:lesson_id/:part/:mark' => 'marks#create'
   get '/students/updatelesson/:id' => 'students#update_lesson', :as => 'student_update'
 
   root :to => 'teachers#index'
   resources :teachers, :only => [:show]
   resources :students, :only => [:show, :index]
   resources :marks
+  resources :lessons, :only => [:index, :show, :edit, :update]
 
 end
